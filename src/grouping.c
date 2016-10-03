95e6aec6c6030f55e5d76fc8a321504629a6832e
anonymous <anonymous>
2016-10-02 21:35:20 -0400

    added more grouping scenarios and streamlines scenarios into a loop test

--- src/grouping.c
+++ src/grouping.c
@@ -1,8 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "grouping.h"
 
-void combine_groups(char numeral[])
-{
-  if(strcmp("VV", numeral) == 0)
- 	strcpy(numeral, "X");
+void combine_groups(char * numeral)
+{ 
+  char buffer[4096];
+  char* location = strstr(numeral, "VV");
+  if(!location)
+    return;  
+
+  strncpy(buffer, numeral, location-numeral);
+  sprintf(buffer+(location-numeral), "%s%s", "X", location + strlen("VV")); 
+  strcpy(numeral, buffer);
 }
 
--- test/grouping_tests.c
+++ test/grouping_tests.c
@@ -1,17 +1,32 @@
 #include <check.h>
 #include "../src/grouping.h"
 
-START_TEST(groups_VV_into_X)
+typedef struct  {
+ char* input;
+ char* expected;
+} InputExpectedPair;
+
+const InputExpectedPair scenarios[] = 
 {
-   char inputOutput[] = "VV";
-   combine_groups(inputOutput);
-   ck_assert_str_eq("X", inputOutput);
+  {"", ""},
+  {"VV", "X"},
+  {"VVII", "XII"}
+
+};
+
+START_TEST(groups_properly)
+{
+  char buffer[4096];
+  strcpy(buffer, scenarios[_i].input);
+  combine_groups(buffer);
+  ck_assert_str_eq(scenarios[_i].expected, buffer);
 }
 END_TEST
+
 
 TCase* grouping_tests(void)
 {
   TCase* grouping = tcase_create("grouping");
-  tcase_add_test(grouping, groups_VV_into_X);
+  tcase_add_loop_test(grouping, groups_properly, 0, sizeof(scenarios)/sizeof(InputExpectedPair));
   return grouping;
 }
