f1bbcbfff0848ca9d7589261ab5a4c5e684d85ab
anonymous <anonymous>
2016-10-11 19:43:44 -0400

    added vlaidation for no more than 3 X

--- src/validate.c
+++ src/validate.c
@@ -7,74 +7,76 @@
 #include "constants.h"
 #include "validate.h"
 
-
 static const char numeral_digits[] = {
- 'I',
- 'V',
- 'X',
- 'L',
- 'C',
- 'D',
- 'M'
-};
+    'I',
+    'V',
+    'X',
+    'L',
+    'C',
+    'D',
+    'M'};
 
 static bool is_character_a_numeral(char numeral_candidate)
 {
-   int i; 
-   for(i=0; i<7; i++)
-   { 
-     if(numeral_candidate == numeral_digits[i])
-     {
-       return true;
-     } 
-   }
-   return false;
-}
-
-static bool is_not_a_numeral(const char* numeral_candidate)
-{
-  for(int char_i=0;char_i<strlen(numeral_candidate);char_i++)
+  int i;
+  for (i = 0; i < 7; i++)
   {
-   if(!is_character_a_numeral(numeral_candidate[char_i]))
-   {
-     return true;
-   } 
+    if (numeral_candidate == numeral_digits[i])
+    {
+      return true;
+    }
   }
   return false;
 }
 
-static bool is_too_long(const char* numeral_candidate)
+static bool is_not_a_numeral(const char *numeral_candidate)
+{
+  for (int char_i = 0; char_i < strlen(numeral_candidate); char_i++)
+  {
+    if (!is_character_a_numeral(numeral_candidate[char_i]))
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+static bool is_too_long(const char *numeral_candidate)
 {
   return strlen(numeral_candidate) > 20;
 }
 
-static bool is_more_than_3_per_IXCM(const char* numeral_candidate)
+static bool is_more_than_3_per_IXCM(const char *numeral_candidate)
 {
-  int count = 0;
-   for(int i=0; i<strlen(numeral_candidate); i++)
-   {
-     if(numeral_candidate[i] == 'I')
-       count++;
-     if(count > 3)
-       return true;
-   }
-   return false;
+  const char no_more_than_3_of_these[] = {'I', 'X'};
+  for (int char_i = 0; char_i < 2; char_i++)
+  {
+    int count = 0;
+    for (int candidate_i = 0; candidate_i < strlen(numeral_candidate); candidate_i++)
+    {
+      if (numeral_candidate[candidate_i] == no_more_than_3_of_these[char_i])
+        count++;
+      if (count > 3)
+        return true;
+    }
+  }
+  return false;
 }
 
-ValidationResult validate_numeral(const char* numeral_candidate)
+ValidationResult validate_numeral(const char *numeral_candidate)
 {
-  if(is_too_long(numeral_candidate))
+  if (is_too_long(numeral_candidate))
     return TOO_LONG;
 
-  if(is_not_a_numeral(numeral_candidate)) 
-     return NOT_A_NUMERAL;
+  if (is_not_a_numeral(numeral_candidate))
+    return NOT_A_NUMERAL;
 
-  if(is_more_than_3_per_IXCM(numeral_candidate))
+  if (is_more_than_3_per_IXCM(numeral_candidate))
     return MORE_THAN_3_PER_IXCM;
   return SUCCESS;
 }
 
 bool validate_buffer(size_t buffer_length)
 {
-   return  MAX_COMBINED_NUMERAL_SIZE <= buffer_length;
+  return MAX_COMBINED_NUMERAL_SIZE <= buffer_length;
 }
